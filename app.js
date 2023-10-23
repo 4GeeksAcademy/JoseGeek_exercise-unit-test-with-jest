754d265cb2afe8debb8e8d66018c2440ecab208b
Jose <126101249+JoseGeek78@users.noreply.github.com>
2023-10-23 19:45:46 +0000

    https://github.com/4GeeksAcademy/JoseGeek_exercise-unit-test-with-jest

--- app.js
+++ app.js
@@ -2,11 +2,7 @@
 const sum = (a,b) => {
     return a + b
 }
-
-
 console.log(sum(7,3))
-
-
 
 let oneEuroIs = {
     "JPY": 127.9, // japan yen
@@ -14,36 +10,20 @@ let oneEuroIs = {
     "GBP": 0.8, // british pound
 }
 
-
-
 const fromEuroToDollar = function(valueInEuro){
-    // convert the given valueInEuro to dollars
     let valueInDollar = valueInEuro * 1.2;
-    //return the dollar value
     return valueInDollar;
 }
 
-
-
-
 const fromDollarToJapanYen = function(valueInDollar){
-    // convert the given valueInDollar to JPY
     let valueInJapanYen = Math.round(valueInDollar * 106.58);
-    //return the JPY value
     return valueInJapanYen;
 }
 
-
-
-
 const fromYenToPounds = function(valueInYen){
-    // convert the given valueInYen to Pounds
     let valueInPounds = Math.round(valueInYen * 0.00625);
-    //return the Pounds value
-    return valueInPounds;
+   return valueInPounds;
 }
-
-
 
 module.exports = { sum, fromEuroToDollar, fromDollarToJapanYen, fromYenToPounds }
 
--- test.js
+++ test.js
@@ -2,57 +2,28 @@
 
 const { sum, fromEuroToDollar, fromDollarToJapanYen, fromYenToPounds } = require('./app.js');
 
-
-
 test('adds 14 + 9 to equal 23', () => {
-    
     let total = sum(14, 9);
-
-    
     expect(total).toBe(23);
 });
 
-
-
 test("One euro should be 1.2 dollars", function(){
-
+  const dollars = fromEuroToDollar(3.5)
+  const expected = 3.5 * 1.2; 
     
-    const dollars = fromEuroToDollar(3.5)
-
-    
-    const expected = 3.5 * 1.2; 
-    
-    
-     expect(fromEuroToDollar(3.5)).toBe(4.2); 
-
+    expect(fromEuroToDollar(3.5)).toBe(4.2); 
 });
-
-
 
 test("One dollar should be 106.58 japan yens", function(){
-
-   
-    const japanYens = fromDollarToJapanYen(3.5)
-
+  const japanYens = fromDollarToJapanYen(3.5)
+  const expected = 3.5 * 106.58; 
     
-    const expected = 3.5 * 106.58; 
-    
-   
-     expect(fromDollarToJapanYen(3.5)).toBe(373)
-
+   expect(fromDollarToJapanYen(3.5)).toBe(373)
 });
 
-
-
 test("One Yen should be 0.00625 Pounds", function(){
-
+   const pounds = fromYenToPounds(3.5)
+   const expected = 3.5 * 0.00625; 
     
-    const pounds = fromYenToPounds(3.5)
-
-    
-    const expected = 3.5 * 0.00625; 
-    
-    
-     expect(fromYenToPounds(3.5)).toBe(0)
-
+   expect(fromYenToPounds(3.5)).toBe(0)
 });
